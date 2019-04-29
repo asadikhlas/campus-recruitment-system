0cf5646eb50a995109aaa761141bd6b793f547e1
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-29 16:49:23 +0500

    commit

--- client/src/components/ReusableComponents/AdminCompanyTable.js
+++ client/src/components/ReusableComponents/AdminCompanyTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {  Table, Button, Icon } from "semantic-ui-react";
+import { Table, Button, Icon } from "semantic-ui-react";
 import Axios from "axios";
 
 class AdminCompanyTable extends Component {
@@ -17,20 +17,22 @@ class AdminCompanyTable extends Component {
     });
   }
 
-  handleRemove = (event) => {
-    Axios.delete("http://localhost:3002/api/company",{data: {email: event.target.name}})
-    .then(res =>{
-      if(res.status === 200){
-        Axios.get("http://localhost:3002/api/company").then(res => {
-          const data = res.data.userData;
-          this.setState({
-            dataSave: data
-          });
-        });
-      }
+  handleRemove = event => {
+    Axios.delete("http://localhost:3002/api/company", {
+      data: { email: event.target.name }
     })
-    .catch(err => alert('Something went wrong'))
-  }
+      .then(res => {
+        if (res.status === 200) {
+          Axios.get("http://localhost:3002/api/company").then(res => {
+            const data = res.data.userData;
+            this.setState({
+              dataSave: data
+            });
+          });
+        }
+      })
+      .catch(err => alert("Something went wrong"));
+  };
 
   render() {
     return (
@@ -63,18 +65,18 @@ class AdminCompanyTable extends Component {
                   <Table.Cell>{item.CompanyCeo}</Table.Cell>
                   <Table.Cell>{item.email}</Table.Cell>
                   <Table.Cell>
-                  <Button
-                    floated="right"
-                    icon
-                    labelPosition="left"
-                    primary
-                    size="small"
-                    name = {item.email}
-                    onClick={this.handleRemove}
-                  >
-                    <Icon name="eraser" /> Remove
-                  </Button>
-                </Table.Cell>
+                    <Button
+                      floated="right"
+                      icon
+                      labelPosition="left"
+                      primary
+                      size="small"
+                      name={item.email}
+                      onClick={this.handleRemove}
+                    >
+                      <Icon name="eraser" /> Remove
+                    </Button>
+                  </Table.Cell>
                 </Table.Row>
               );
             })}
