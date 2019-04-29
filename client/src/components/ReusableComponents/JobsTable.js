db36c2fa23cb14d545b6fcb43bfaff18f2020478
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-29 16:47:59 +0500

    completed

--- client/src/components/ReusableComponents/JobsTable.js
+++ client/src/components/ReusableComponents/JobsTable.js
@@ -5,7 +5,7 @@ import Axios from "axios";
 class JobsTable extends Component {
   state = {
     dataSave: [],
-    applied:false
+    applied: false
   };
 
   componentDidMount() {
@@ -19,61 +19,63 @@ class JobsTable extends Component {
   }
 
   handleEvent = () => {
-      alert('you applied on this job successfully')
-        this.setState({
-            applied:true
-        })
-
-     }
+    alert("you applied on this job successfully");
+    this.setState({
+      applied: true
+    });
+  };
 
   render() {
     return (
-        <div>
-            <h2 style={{
-                textAlign:"center"
-            }}>Posted Jobs Information</h2>
-      <Table compact celled definition>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell />
+      <div>
+        <h2
+          style={{
+            textAlign: "center"
+          }}
+        >
+          Posted Jobs Information
+        </h2>
+        <Table compact celled definition>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell>Job Title</Table.HeaderCell>
+              <Table.HeaderCell>Company Name</Table.HeaderCell>
+              <Table.HeaderCell>Job Description</Table.HeaderCell>
+              <Table.HeaderCell>Salary</Table.HeaderCell>
+              <Table.HeaderCell>Date</Table.HeaderCell>
+              <Table.HeaderCell>Applid To</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-            <Table.HeaderCell>Job Title</Table.HeaderCell>
-            <Table.HeaderCell>Company Name</Table.HeaderCell>
-            <Table.HeaderCell>Job Description</Table.HeaderCell>
-            <Table.HeaderCell>Salary</Table.HeaderCell>
-            <Table.HeaderCell>Date</Table.HeaderCell>
-            <Table.HeaderCell>Applid To</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {this.state.dataSave.map(item => {
-            return (
-              <Table.Row key={item._id}>
-                <Table.Cell collapsing />
-                <Table.Cell>{item.title}</Table.Cell>
-                <Table.Cell>{item.companyName}</Table.Cell>
-                <Table.Cell>{item.description}</Table.Cell>
-                <Table.Cell>{item.salary}</Table.Cell>
-                <Table.Cell>{item.date}</Table.Cell>
-                <Table.Cell>
-                  <Button
-                    floated="right"
-                    icon
-                    labelPosition="left"
-                    primary
-                    size="small"
-                    onClick={this.handleEvent}
-                    disabled = {this.state.applied ? true : false}
-                  >
-                    <Icon name="eraser" /> Apply
-                  </Button>
-                </Table.Cell>
-              </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
+          <Table.Body>
+            {this.state.dataSave.map(item => {
+              return (
+                <Table.Row key={item._id}>
+                  <Table.Cell collapsing />
+                  <Table.Cell>{item.title}</Table.Cell>
+                  <Table.Cell>{item.companyName}</Table.Cell>
+                  <Table.Cell>{item.description}</Table.Cell>
+                  <Table.Cell>{item.salary}</Table.Cell>
+                  <Table.Cell>{item.date}</Table.Cell>
+                  <Table.Cell>
+                    <Button
+                      floated="right"
+                      icon
+                      labelPosition="left"
+                      primary
+                      size="small"
+                      onClick={this.handleEvent}
+                      disabled={this.state.applied ? true : false}
+                    >
+                      <Icon name="eraser" /> Apply
+                    </Button>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
       </div>
     );
   }
